78f788d63e0d5aa85a6e558aac54b106d5ba0e27
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-03-27 21:02:02 +0100

    Cleaned Up Analysis folder

--- analysis/streaming/test.c
+++
@@ -1,7 +0,0 @@
-#include <emscripten.h>
-
-EMSCRIPTEN_KEEPALIVE
-int sumOfNInts(int n)
-{
-    return n;
-}
--- analysis/wasmCompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'Calling emcc...'
-
-emcc main.cpp -o analysis.js -s NO_EXIT_RUNTIME=1 -s EXPORTED_RUNTIME_METHODS=ccall,cwrap
-
-echo 'Success!'
